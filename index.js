67cc49758847661e2e2e2b07a8e4354284a5ccf6
shubhamprakash911 <shubhamprakash9112000@gmail.com>
2023-06-29 01:06:27 +0530

    correction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const axios = require('axios');
+const { OpenAIApi, Configuration } = require('openai');
 require('dotenv').config();
+
 const cors = require("cors")
 
 const app = express();
@@ -13,23 +14,38 @@ app.get("/",(req,res)=>{
   res.send({"msg":"backend of joke_gpt"})
 })
 
+// Set up OpenAI configuration
+const openaiConfig = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(openaiConfig);
+const history = []
+// Endpoint to generate a joke based on the provided keyword
 app.get('/joke', async (req, res) => {
-  try {
-    const keyword = req.query.keyword;
-    const response = await axios.post('https://api.openai.com/v1/engines/davinci/completions', {
-      prompt: `make a funny joke on ${keyword}`,
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    });
+  const user_input = `Make a funny joke  on ${req.query.keyword}`
+  const messages = [];
+  for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
+  }
 
-    const shayari = response.data.choices[0].text.trim();
-    res.json({ shayari });
+  messages.push({ role: "user", content: user_input });
+
+  try {
+      // Customize this based on your desired prompt and completion settings
+      // console.log(messages);
+      const completion = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: messages,
+      });
+
+      const completion_text = completion.data.choices[0].message.content;
+      // console.log(completion_text);
+      history.push([user_input, completion_text]);
+      res.json({ shayari: completion_text });
   } catch (error) {
-    console.error('Error:', error.response.data);
-    res.status(500).json({ error: 'Something went wrong' });
+      console.error('OpenAI API request failed:', error);
+      res.status(500).json({ error: 'Failed to generate joke' });
   }
 });
 
